d417b557e93227295a56475d4ce962ab012f1e82
DrKodak <dr.kodak.md@gmail.com>
2023-10-24 21:15:25 -0500

    initial commit, working on templating

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// func getUser(c echo.Context) error {
+// 	// User ID From path `users/:id`
+// 	id := c.Param("id")
+// 	return c.String(http.StatusOK, id)
+// }
+
+// func show(c echo.Context) error {
+// 	team := c.QueryParam("team")
+// 	member := c.QueryParam("member")
+// 	return c.String(http.StatusOK, "team: " + team + ", member: " + member)
+// }
+
+// func save(c echo.Context) error {
+// 	// get name and email
+// 	name := c.FormValue("name")
+// 	email := c.FormValue("email")
+// 	return c.String(http.StatusOK, "name: " + name + "email: " + email)
+// }
+
+func TimeLoggingMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		start := time.Now()
+		err := next(c)
+		duration := time.Since(start)
+		log.Printf("Processed request in %s", duration)
+		return err
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Static("/static", "static")
+
+	// Root level middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello life!")
+	})
+
+	e.POST("/clicked", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, `<p>You clicked me!</p>`)
+	})
+	// e.POST("/users", saveUser)
+	// e.GET("/users/:id", getUser)
+	// e.GET("/show", show)
+	// e.POST("/save", save)
+	// e.PUT("/users/:id", updateUser)
+
+	// e.DELETE("users/:id", deleteUser)
+	e.Logger.Fatal(e.Start(":1323"))
+}
---
+++ rules.md
@@ -0,0 +1,12 @@
+Rules
+
+For populated spaces:
+Each cell with one or no neighbors dies.
+Each cell with four or more neighbors dies.
+Each cell with two or three neighbors survives.
+
+For empty spaces:
+Each cell with three neighbors becomes populated.
+
+links:
+https://medium.com/gravel-engineering/this-blogpost-also-posted-in-my-personal-blog-which-you-can-access-here-dd856c61001
